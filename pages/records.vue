895b97fcd9b6822ea4fd61fe86bd540f7e731e08
limingtao <limingtao@7ipr.com>
2018-04-02 15:57:25 +0800

    show img list

--- pages/records.vue
+++ pages/records.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container" v-loading.fullscreen.lock="loading" element-loading-text="拼命加载中" element-loading-spinner="el-icon-loading" element-loading-background="rgba(0, 0, 0, 0.8)">
-    <el-table :data="recordList" :header-cell-style="headerCell" style="width: 100%" height="500">
+    <el-table :data="recordList" :header-cell-style="headerCell" style="width: 100%" height="600">
       <el-table-column fixed prop="from_name" label="发送人" width="100%">
       </el-table-column>
       <el-table-column prop="to_name" label="接受者" width="300">
@@ -33,6 +33,9 @@ export default {
     api.getRecords().then((res) => {
       res.data.map((e) => {
         e.createdAt = moment(e.createdAt).format("YYYY-MM-DD HH:mm:ss")
+        if (e.type == "media") {
+          e.content = <img src={e.content} />
+        }
       })
       this.recordList = res.data
     })
